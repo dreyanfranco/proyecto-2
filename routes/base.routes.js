b48e2bf2e500cde3c5a57680bf4650646ca4ac86
dreyanfranco <dreyanfranco@gmail.com>
2020-11-23 17:18:38 +0100

    vista de perfil y ruta admin

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -6,6 +6,6 @@ const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(re
 // Endpoints
 router.get('/', (req, res) => res.render('index'))
 
-router.get('/perfil', ensureAuthenticated, checkRole(['ADMIN', 'USER']), (req, res) => res.render('profile', {user: req.user}))
+router.get('/perfil', ensureAuthenticated, checkRole(['ADMIN', 'USER']), (req, res) => res.render('profile', { user: req.user, isAdmin: req.user.role.includes('ADMIN') }))
 
 module.exports = router
