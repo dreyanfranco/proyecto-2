bb3e3b529ec0368e2ced6e7e1d5d6f475f5f769a
Isabel Perez <isabel.perez.ruiz25@gmail.com>
2020-11-24 10:56:43 +0100

    cdn images

--- routes/plants.routes.js
+++ routes/plants.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Plant = require('../models/plants.model');
+const CDNupload = require('./../configs/cdn-upload.config')
+
+
 //Listado de plantas
 router.get('/', (req, res, next) => { 
     Plant.find()
@@ -14,9 +17,10 @@ router.get('/crear-planta', (req, res, next) => {
     .then(allThePlants => res.render('plants/new-plant', { plants: allThePlants}))
     .catch(err => next(new Error(err)))
 })
-router.post('/crear-planta', (req, res, next) => {
-
-    const { name, scientificName, imageUrl, description, climate, heigth, water, spray, care, ligth, location, petFriendly} = req.body
+router.post('/crear-planta', CDNupload.single('imageUrl'), (req, res, next) => {
+    
+    const imageUrl= req.file.path
+    const { name, scientificName , description, climate, heigth, water, spray, care, ligth, location, petFriendly} = req.body
 
      Plant
         .create({ name, scientificName, imageUrl, description, climate, heigth, water, spray, care, ligth, location, petFriendly })
